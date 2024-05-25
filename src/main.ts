6005edaf3b80e5fe7d794b96e0ee3a058d84d924
nicofishman <nicourifishman@gmail.com>
2024-05-25 19:09:07 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as YAML from 'json-to-pretty-yaml';
+import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  fs.writeFileSync('./swagger.yaml', YAML.stringify(document));
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
