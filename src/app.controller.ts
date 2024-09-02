df41c4ab8f597d979351eb237d0785bd14bd4f67
nicofishman <nicourifishman@gmail.com>
2024-09-02 20:04:10 -0300

    feat: remove app things not needed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,6 @@
-import { Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  @Get() getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/test/:id') test(@Param('id') id: string): { res: string } {
-    return { res: 'test' + id };
-  }
-
-  @Post('/test/:id') testPost(@Param('id') id: string): { res: string } {
-    return { res: 'test' + id };
-  }
-
-  @Put('/test/:id') testPut(@Param('id') id: string): { res: string } {
-    return { res: 'test' + id };
-  }
-
-  @Post('/otraRuta/:id1/:id2') otraRuta(
-    @Param('id1') id1: string,
-    @Param('id2') id2: string,
-  ): { res: string } {
-    return { res: 'otraRuta' + id1 + id2 };
-  }
 }
