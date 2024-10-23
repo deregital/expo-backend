3d4f3640637a0d6cdfe69d12b921dff3da0913cc
Alan Babor <baboralanoroynegro@gmail.com>
2024-10-23 18:03:08 -0300

    add: cannedResponse.module

--- src/cannedResponse/cannedResponse.module.ts
+++ src/cannedResponse/cannedResponse.module.ts
@@ -1,9 +1,12 @@
+import { AccountService } from '@/account/account.service';
+import { PrismaService } from '@/prisma/prisma.service';
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { CannedResponseController } from './cannedResponse.controller';
 import { CannedResponseService } from './cannedResponse.service';
 
 @Module({
-  providers: [CannedResponseService],
+  providers: [CannedResponseService, PrismaService, JwtService, AccountService],
   controllers: [CannedResponseController],
 })
 export class CannedResponseModule {}
