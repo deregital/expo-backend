48b10d2242dfa8f976bb614484875ead6459cd99
Alan Babor <baboralanoroynegro@gmail.com>
2024-10-23 18:05:00 -0300

    add: cannedResponse.service

--- src/cannedResponse/cannedResponse.service.ts
+++ src/cannedResponse/cannedResponse.service.ts
@@ -1,4 +1,72 @@
+import {
+  CreateCannedResponseDto,
+  createCannedResponseResponseSchema,
+} from './dto/create-cannedResponse.dto';
+import { deleteCannedResponseResponseSchema } from './dto/delete-cannedResponse.dto';
+import { UpdateCannedResponseDto } from './dto/update-cannedResponse.dto';
+
+import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import z from 'zod';
+import { Account, CannedResponse } from '~/types';
 
 @Injectable()
-export class CannedResponseService {}
+export class CannedResponseService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createCannedResponse(
+    dto: CreateCannedResponseDto,
+    accountId: Account['id'],
+  ): Promise<CannedResponse> {
+    return await this.prisma.cannedResponse.create({
+      data: {
+        name: dto.name,
+        content: dto.content,
+      },
+    });
+  }
+
+  async getAllCannedResponses(): Promise<
+    z.infer<typeof createCannedResponseResponseSchema>
+  > {
+    const cannedResponses = await this.prisma.cannedResponse.findMany({
+      orderBy: {
+        created_at: 'desc',
+      },
+    });
+
+    const response = createCannedResponseResponseSchema.parse({
+      cannedResponses,
+    });
+
+    return response;
+  }
+
+  async updateCannedResponse(
+    id: string,
+    dto: UpdateCannedResponseDto,
+  ): Promise<CannedResponse> {
+    return await this.prisma.cannedResponse.update({
+      where: { id },
+      data: {
+        name: dto.name,
+        content: dto.content,
+      },
+    });
+  }
+
+  async deleteCannedResponse(
+    id: string,
+  ): Promise<z.infer<typeof deleteCannedResponseResponseSchema>> {
+    const deletedResponse = await this.prisma.cannedResponse.delete({
+      where: { id },
+    });
+    return {
+      id: deletedResponse.id,
+      name: deletedResponse.name,
+      content: deletedResponse.content,
+      created_at: deletedResponse.created_at,
+      updated_at: deletedResponse.updated_at,
+    };
+  }
+}
