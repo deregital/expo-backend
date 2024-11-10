a1e03e8d4c9415d30f99ec60795301a9c6f0fbfc
nicofishman <74215078+nicofishman@users.noreply.github.com.>
2024-11-10 15:10:03 -0300

    fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,8 +4,6 @@ import { PrismaClient } from '~/types/prisma-schema';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit(): Promise<void> {
-    console.log('PrismaService initialized');
-
     await this.$connect();
   }
 }
