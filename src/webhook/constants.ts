e6df8f046bf49f0c1190234d23c49f258aa39f1e
nicofishman <74215078+nicofishman@users.noreply.github.com.>
2024-11-25 12:10:04 -0300

    feat: comment explaining "random" constant

--- src/webhook/constants.ts
+++ src/webhook/constants.ts
@@ -1,1 +1,2 @@
+// Esta constante define la fecha límite para que los mensajes automáticos se envíen a los usuarios. Los usuarios creados antes de esta fecha no recibirán mensajes automáticos.
 export const AUTOMATIC_MESSAGE_DEADLINE = new Date(2024, 4, 10);
