79ef561026a0a143a00a6c90fcaadf0623bf29a6
nicofishman <74215078+nicofishman@users.noreply.github.com.>
2025-02-19 16:18:07 -0300

    fix: add birth and residence location to getMiExpoMe response type

--- src/mi-expo/dto/get-me.dto.ts
+++ src/mi-expo/dto/get-me.dto.ts
@@ -1,7 +1,13 @@
+import { locationSchema } from '@/schema/location.schema';
 import { profileSchema } from '@/schema/profile.schema';
 import { createZodDtoWithoutDate } from '@/shared/dto-modification/create-zod-dto-without-date';
 
-export const getMiExpoMeResponseSchema = profileSchema.omit({ password: true });
+export const getMiExpoMeResponseSchema = profileSchema
+  .omit({ password: true })
+  .extend({
+    residenceLocation: locationSchema.nullable(),
+    birthLocation: locationSchema.nullable(),
+  });
 
 export class GetMiExpoMeResponseDto extends createZodDtoWithoutDate(
   getMiExpoMeResponseSchema,
