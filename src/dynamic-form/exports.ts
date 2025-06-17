9389674ea54f73d7ca736d83e92e9be07588e854
Sebastian Biorci <sebipochi@gmail.com>
2025-06-17 14:42:54 -0300

    add exports

--- src/dynamic-form/exports.ts
+++ src/dynamic-form/exports.ts
@@ -2,4 +2,5 @@ export * from './dto/create-dynamic-form.dto';
 export * from './dto/delete-dynamic-form.dto';
 export * from './dto/dynamic-form.dto';
 export * from './dto/find-all-dynamic-form.dto';
+export * from './dto/submit-dynamic-form.dto';
 export * from './dto/update-dynamic-form.dto';
