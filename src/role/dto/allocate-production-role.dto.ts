ae3f2ae483a3b73c369ec55cbe048082986ef3d8
Sebastian Biorci <sebipochi@gmail.com>
2025-06-29 03:48:25 -0300

    fix: restore schemas

--- src/role/dto/allocate-production-role.dto.ts
+++ src/role/dto/allocate-production-role.dto.ts
@@ -1,5 +1,14 @@
 import { profileSchema } from '@/schema/profile.schema';
 import { createZodDtoWithoutDate } from '@/shared/dto-modification/create-zod-dto-without-date';
+import z from 'zod';
+
+export const allocateProductionRoleSchema = z.object({
+  profileId: profileSchema.shape.id,
+});
+
+export class AllocateProductionRoleDto extends createZodDtoWithoutDate(
+  allocateProductionRoleSchema,
+) {}
 
 export const allocateProductionRoleResponseSchema = profileSchema.omit({
   password: true,
