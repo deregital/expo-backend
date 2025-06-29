9fcdb8d807c8500b3f1bc2127401dda0893f8ff8
Sebastian Biorci <sebipochi@gmail.com>
2025-06-29 02:00:46 -0300

    add exports

--- src/role/exports.ts
+++ src/role/exports.ts
@@ -1,3 +1,5 @@
+export * from './dto/allocate-participant-role.dto';
+export * from './dto/allocate-production-role.dto';
 export * from './dto/create-role.dto';
 export * from './dto/delete-role.dto';
 export * from './dto/find-all-role.dto';
