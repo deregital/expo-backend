d12d6a8245d61cec1a477596fe23d668fb500dfc
Sebastian Biorci <sebipochi@gmail.com>
2025-06-29 03:55:00 -0300

    add schemas

--- src/role/dto/deallocate-all-role.dto.ts
+++ src/role/dto/deallocate-all-role.dto.ts
@@ -1,5 +1,14 @@
 import { profileSchema } from '@/schema/profile.schema';
 import { createZodDtoWithoutDate } from '@/shared/dto-modification/create-zod-dto-without-date';
+import z from 'zod';
+
+export const deallocateAllRoleSchema = z.object({
+  profileId: profileSchema.shape.id,
+});
+
+export class DeallocateAllRoleDto extends createZodDtoWithoutDate(
+  deallocateAllRoleSchema,
+) {}
 
 export const deallocateAllRoleResponseSchema = profileSchema.omit({
   password: true,
